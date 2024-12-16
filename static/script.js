8aa5f7740b9abdde4b5dff7a40902033b98c22b7
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2024-12-16 17:25:52 -0300

    Done the header section
    + Almost of the header section already translated

--- static/script.js
+++ static/script.js
@@ -5,6 +5,12 @@ const body = document.querySelector('body')
 
 const header = document.querySelector('#header')
     const home = document.querySelector('#home')
+    const about = document.querySelector('#about')
+    const abilities = document.querySelector('#abilities')
+    const certificates = document.querySelector('#certificates')
+    const portfolio = document.querySelector('#portfolio')
+    const contact = document.querySelector('#contact')
+    const languageButton = document.querySelector('#changeLanguage')
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
     ? menuMobile.classList.replace("bi-list", "bi-x")
@@ -23,6 +29,12 @@ navItem.forEach(item => {
     })
 })
 
-function changeLanguage(){
+function changeLanguage() {
+    languageButton.innerText = "Change Language";
     home.innerHTML = '<i class="bi bi-house-door"></i>Home'
+    about.innerHTML = '<i class="bi bi-person-fill"></i> About'
+    abilities.innerHTML = '<i class="bi bi-book"></i> Abilities'
+    certificates.innerHTML = '<i class="bi bi-journal"></i> Certificates'
+    portfolio.innerHTML = '<i class="bi bi-code-square"></i> Portfolio'
+    contact.innerHTML = '<i class="bi bi-envelope"></i> Contact'
 }
